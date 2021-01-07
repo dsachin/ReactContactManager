61871946030cf2618ea5a904c2f4d440838074e9
Sachin Diwate <schndiwate@gmail.com>
2021-01-08 00:53:03 +0530

    Fix issue when clicked on branding showing 404.

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header(props) {
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
             <div className="container">
-                <a href="/" className="navbar-brand">{branding}</a>
+                <Link to="/" > <span className="navbar-brand">{branding}</span></Link>
             </div>
             <ul className="navbar-nav mr-auto">
                 <li className="nav-item">
